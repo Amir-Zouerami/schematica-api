506b4875d031a2f52844f26e37f5d1006e4a2dfc
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2025-10-27 22:33:56 +0330

    EXCEPTION: added the project not found exception.

---
+++ src/common/exceptions/project-not-found.exception.ts
@@ -0,0 +1,12 @@
+import { HttpStatus } from '@nestjs/common';
+import { BaseAppException } from './base-app.exception';
+
+export class ProjectNotFoundException extends BaseAppException {
+	constructor(projectId: string) {
+		super(
+			`Project with ID "${projectId}" not found or you do not have permission to view it.`,
+			HttpStatus.NOT_FOUND,
+			'PROJECT_NOT_FOUND',
+		);
+	}
+}
