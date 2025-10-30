6a9dae8f8c62b3f4378eea1e3590d3d6601399ac
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2025-10-31 00:06:38 +0330

    TEST: mocked some tests.

--- src/projects/notes/notes.controller.spec.ts
+++ src/projects/notes/notes.controller.spec.ts
@@ -1,12 +1,25 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test, type TestingModule } from '@nestjs/testing';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ProjectViewerGuard } from '../guards/project-viewer.guard';
+import { NotePermissionGuard } from './guards/note-permission.guard';
 import { NotesController } from './notes.controller';
+import { NotesService } from './notes.service';
 
 describe('NotesController', () => {
 	let controller: NotesController;
 
+	const mockNotesService = {};
+	const mockPrismaService = {};
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [NotesController],
+			providers: [
+				{ provide: NotesService, useValue: mockNotesService },
+				NotePermissionGuard,
+				ProjectViewerGuard,
+				{ provide: PrismaService, useValue: mockPrismaService },
+			],
 		}).compile();
 
 		controller = module.get<NotesController>(NotesController);
