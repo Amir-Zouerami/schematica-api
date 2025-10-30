2e500b45a0a91ce839fedc9c5d8c027755723b77
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2025-10-31 00:43:52 +0330

    TEST: fixed the tests and incorrect module imports.

--- src/projects/endpoints/endpoints.module.ts
+++ src/projects/endpoints/endpoints.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { NotesController } from '../notes/notes.controller';
-import { NotesModule } from '../notes/notes.module';
-import { NotesService } from '../notes/notes.service';
 import { EndpointsController } from './endpoints.controller';
 import { EndpointsService } from './endpoints.service';
 
 @Module({
-	imports: [PrismaModule, NotesModule],
-	controllers: [EndpointsController, NotesController],
-	providers: [EndpointsService, NotesService],
+	imports: [PrismaModule],
+	controllers: [EndpointsController],
+	providers: [EndpointsService],
 })
 export class EndpointsModule {}
--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -5,14 +5,15 @@ import { AuthModule } from 'src/auth/auth.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { EndpointsModule } from './endpoints/endpoints.module';
 import { ProjectOwnerGuard } from './guards/project-owner.guard';
+import { NotesModule } from './notes/notes.module';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
-import { SpecReconciliationService } from './spec-reconciliation/spec-reconciliation.service';
 import { OpenApiSpecBuilder } from './spec-builder/openapi-spec.builder';
+import { SpecReconciliationService } from './spec-reconciliation/spec-reconciliation.service';
 import { AreLinksUniqueConstraint } from './validators/are-links-unique.validator';
 
 @Module({
-	imports: [PrismaModule, AuthModule, EndpointsModule],
+	imports: [PrismaModule, AuthModule, EndpointsModule, NotesModule],
 	controllers: [ProjectsController],
 	providers: [
 		ProjectsService,
