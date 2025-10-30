aa56d3ea11af359f1d18bb2f7613182f2ca9b41b
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2025-10-31 00:06:38 +0330

    TEST: mocked some tests.

--- src/projects/notes/notes.service.spec.ts
+++ src/projects/notes/notes.service.spec.ts
@@ -1,12 +1,26 @@
-import { Test, TestingModule } from '@nestjs/testing';
+// Path: src/projects/notes/notes.service.spec.ts
+
+import { Test, type TestingModule } from '@nestjs/testing';
+import { PinoLogger } from 'nestjs-pino';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { NotesService } from './notes.service';
 
 describe('NotesService', () => {
 	let service: NotesService;
 
+	const mockPrismaService = {};
+	const mockPinoLogger = {
+		setContext: jest.fn(),
+		error: jest.fn(),
+	};
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [NotesService],
+			providers: [
+				NotesService,
+				{ provide: PrismaService, useValue: mockPrismaService },
+				{ provide: PinoLogger, useValue: mockPinoLogger },
+			],
 		}).compile();
 
 		service = module.get<NotesService>(NotesService);
